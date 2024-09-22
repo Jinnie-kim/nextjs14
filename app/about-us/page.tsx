825f5ccdb6c2b22a4ddff7d8a02c3c46d6a87408
Jinnie <hjk7639@gmail.com>
2024-09-22 11:52:25 +0900

    Add Layouts

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -1,9 +1,6 @@
-import Navigation from '../../components/navigation';
-
 export default function About() {
   return (
     <div>
-      <Navigation />
       <h1>This is About page</h1>
     </div>
   );

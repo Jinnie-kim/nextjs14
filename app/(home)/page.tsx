dce3ac8c32129dd36980a233f0eb4dfa348b3be6
Jinnie <hjk7639@gmail.com>
2024-09-24 21:20:19 +0900

    Fetching Data: server side

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,25 +1,18 @@
-'use client';
+export const metadata = {
+  title: 'Home',
+};
 
-import { useEffect, useState } from 'react';
+const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
 
-export default function Page() {
-  const [isLoading, setIsLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
+async function getMovies() {
+  return fetch(URL).then((response) => response.json());
+}
 
-  const getMovies = async () => {
-    const response = await fetch('https://nomad-movies.nomadcoders.workers.dev/movies');
-    const json = await response.json();
-    setMovies(json);
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    getMovies();
-  }, []);
-
+export default async function HomePage() {
+  const movies = await getMovies();
   return (
     <div>
-      <h1>{isLoading ? 'Loading....' : JSON.stringify(movies)}</h1>
+      <h1>{JSON.stringify(movies)}</h1>
     </div>
   );
 }
